d382cf80cc16b535c613002dba9384484a8c6fca
_xeroxz <xerox@back.engineering>
2020-11-26 18:49:03 -0800

    its not stable but git is being impossible to work with so im just pushing this shit to master and going 2 bed, fuck it!

--- PSKDM/mem_ctx/mem_ctx.hpp
+++ PSKDM/mem_ctx/mem_ctx.hpp
@@ -1,36 +1,33 @@
 #pragma once
 #include "../util/nt.hpp"
-#include "../vdm_ctx/vdm_ctx.h"
+#include "../vdm_ctx/vdm_ctx.hpp"
 
 namespace nasa
 {
 	class mem_ctx
 	{
-		friend class mapper_ctx;
 	public:
-		explicit mem_ctx(vdm::vdm_ctx& v_ctx, DWORD pid = GetCurrentProcessId());
+		explicit mem_ctx(vdm::vdm_ctx* v_ctx, std::uint32_t pid = GetCurrentProcessId());
 		~mem_ctx();
 
-		std::pair<ppte, pte> get_pte(void* addr, bool use_hyperspace = false);
-		void set_pte(void* addr, const ::pte& pte, bool use_hyperspace = false);
+		auto get_pte(void* addr, bool use_hyperspace = false) -> std::pair<ppte, pte>;
+		bool set_pte(void* addr, const ::pte& pte, bool use_hyperspace = false);
 
-		std::pair<ppde, pde> get_pde(void* addr, bool use_hyperspace = false);
-		void set_pde(void* addr, const ::pde& pde, bool use_hyperspace = false);
+		auto get_pde(void* addr, bool use_hyperspace = false) -> std::pair<ppde, pde>;
+		bool set_pde(void* addr, const ::pde& pde, bool use_hyperspace = false);
 
-		std::pair<ppdpte, pdpte> get_pdpte(void* addr, bool use_hyperspace = false);
-		void set_pdpte(void* addr, const ::pdpte& pdpte, bool use_hyperspace = false);
+		auto get_pdpte(void* addr, bool use_hyperspace = false) -> std::pair<ppdpte, pdpte>;
+		bool set_pdpte(void* addr, const ::pdpte& pdpte, bool use_hyperspace = false);
 
-		std::pair<ppml4e, pml4e> get_pml4e(void* addr, bool use_hyperspace = false);
-		void set_pml4e(void* addr, const ::pml4e& pml4e, bool use_hyperspace = false);
-
-		void* get_dirbase() const;
+		auto get_pml4e(void* addr, bool use_hyperspace = false) -> std::pair<ppml4e, pml4e>;
+		bool set_pml4e(void* addr, const ::pml4e& pml4e, bool use_hyperspace = false);
 		static void* get_dirbase(vdm::vdm_ctx& v_ctx, DWORD pid);
 
-		void read_phys(void* buffer, void* addr, std::size_t size);
-		void write_phys(void* buffer, void* addr, std::size_t size);
+		bool read_phys(void* buffer, void* addr, std::size_t size);
+		bool write_phys(void* buffer, void* addr, std::size_t size);
 
 		template <class T>
-		T read_phys(void* addr)
+		__forceinline T read_phys(void* addr)
 		{
 			T buffer;
 			read_phys((void*)&buffer, addr, sizeof(T));
@@ -38,13 +35,13 @@ namespace nasa
 		}
 
 		template <class T>
-		void write_phys(void* addr, const T& data)
+		__forceinline bool write_phys(void* addr, const T& data)
 		{
-			write_phys((void*)&data, addr, sizeof(T));
+			return write_phys((void*)&data, addr, sizeof(T));
 		}
 
-		std::pair<void*, void*> read_virtual(void* buffer, void* addr, std::size_t size);
-		std::pair<void*, void*> write_virtual(void* buffer, void* addr, std::size_t size);
+		auto read_virtual(void* buffer, void* addr, std::size_t size) -> std::pair<void*, void*>;
+		auto write_virtual(void* buffer, void* addr, std::size_t size) -> std::pair<void*, void*>;
 
 		template <class T>
 		__forceinline T read_virtual(void* addr)
@@ -61,19 +58,15 @@ namespace nasa
 		}
 
 		void* virt_to_phys(pt_entries& entries, void* addr);
+		bool hyperspace_entries(pt_entries& entries, void* addr);
+
 		void* set_page(void* addr);
 		void* get_page() const;
-		unsigned get_pid() const;
 
-		pml4e operator[](std::uint16_t pml4_idx);
-		pdpte operator[](const std::pair<std::uint16_t, std::uint16_t>& entry_idx);
-		pde operator[](const std::tuple<std::uint16_t, std::uint16_t, std::uint16_t>& entry_idx);
-		pte operator[](const std::tuple<std::uint16_t, std::uint16_t, std::uint16_t, std::uint16_t>& entry_idx);
-	private:
-
-		bool hyperspace_entries(pt_entries& entries, void* addr);
+		unsigned pid;
 		void* dirbase;
 		vdm::vdm_ctx* v_ctx;
+	private:
 		std::uint16_t pml4e_index, 
 			pdpte_index,
 			pde_index, 
@@ -83,6 +76,5 @@ namespace nasa
 		std::pair<ppdpte, ppdpte> new_pdpt;
 		std::pair<ppde,ppde>      new_pd;
 		std::pair<ppte, ppte>     new_pt;
-		unsigned pid;
 	};
 }
--- um-example/main.cpp
+++ um-example/main.cpp
@@ -20,10 +20,10 @@ int __cdecl main(int argc, char** argv)
         (
             driver_data.data(),
             driver_data.size(),
-            nullptr
+            nullptr // you can pass your structure here...
         );
 
-    std::printf("[+] driver mapping result -> 0x%x\n", result);
+    std::printf("[+] driver mapping result -> 0x%x (0 == STATUS_SUCCESS)\n", result);
     std::printf("[+] driver base address (usermode) -> 0x%p\n", driver_base);
     std::getchar();
 }
